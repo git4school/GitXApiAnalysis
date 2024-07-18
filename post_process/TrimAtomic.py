e297fae39674a3548ca0aec588f0c222287a3fcc
Wyrdix <wyrdix@gmail.com>
2024-07-18 09:59:17 +0200

    FIx atomic not being trimed

--- post_process/TrimAtomic.py
+++ post_process/TrimAtomic.py
@@ -39,6 +39,44 @@ class TrimAtomic(PostProcessModifier):
             if differentials[i].parts == None:
                 continue
             for j in range(len(differentials[i].parts)):
+                if differentials[i].parts[j] == None:
+                    continue
                 differentials[i].parts[j] = PostProcessModifier.trim_diff(
                     differentials[i].parts[j]
                 )
+
+    def process(self, statements: list[Statement]):
+
+        i = 0
+
+        elements = [(s, True) for s in statements]
+
+        while i < len(statements):
+
+            to_process = elements[i][1]
+
+            if not to_process:
+                i += 1
+                continue
+
+            l = self._process(
+                lambda x: elements[x][0] if 0 <= x < len(elements) else None, i
+            )
+
+            if l == None:
+                l = [(elements[i][0], False)]
+            elements = elements[:i] + l + elements[i + 1 :]
+        elements = [e[0] for e in elements]
+
+        return [
+            e
+            for e in elements
+            if not "git" in e.object.definition.extensions
+            or any(
+                [
+                    any([p != None for p in diff.parts])
+                    for diff in e.object.definition.extensions["git"]
+                    if diff != None and diff.parts != None
+                ]
+            )
+        ]
