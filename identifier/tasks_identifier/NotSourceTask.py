d76d227155fad9c2e6dd4b64dc8c3b2e221b5d8c
Wyrdix <wyrdix@gmail.com>
2024-07-22 12:11:29 +0200

    Add NotSourceTask

---
+++ identifier/tasks_identifier/NotSourceTask.py
@@ -0,0 +1,17 @@
+from typing import Callable
+from tincan import Statement
+from gittoxapi.differential import Differential
+from identifier.tasks_identifier.TaskIdentifier import TaskIdentifier
+
+
+class NotSourceTask(TaskIdentifier):
+
+    def generator_prefix(self) -> str:
+        return "notsource"
+
+    def process_differential(
+        self, st_getter: any, i: int, diff: Differential
+    ) -> Callable[[Statement], None]:
+        if diff.file.endswith(".java"):
+            return
+        return TaskIdentifier.task_applier("NotSource", {})
