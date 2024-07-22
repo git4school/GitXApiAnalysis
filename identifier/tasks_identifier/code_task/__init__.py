96aa3f523161d77c000f47de925c376675436f18
Wyrdix <wyrdix@gmail.com>
2024-07-22 15:16:45 +0200

    Identify line break and spacing change

--- identifier/tasks_identifier/code_task/__init__.py
+++ identifier/tasks_identifier/code_task/__init__.py
@@ -1,3 +1,6 @@
 from identifier.tasks_identifier.code_task.CodeTaskIdentifier import CodeTaskIdentifier
 from identifier.tasks_identifier.code_task.PackageTask import PackageTask
 from identifier.tasks_identifier.code_task.ImportTask import ImportTask
+from identifier.tasks_identifier.code_task.LineBreakAndSpacingChangeTask import (
+    LineBreakAndSpacingChangeTask,
+)
