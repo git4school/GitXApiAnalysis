cd4b63528b34dd052d9d9876323641d927e462fe
Wyrdix <wyrdix@gmail.com>
2024-07-23 13:32:20 +0200

    Fix NotSource not working when having multiple files

--- identifier/tasks_identifier/NotSourceTask.py
+++ identifier/tasks_identifier/NotSourceTask.py
@@ -2,6 +2,8 @@ from typing import Callable
 from tincan import Statement
 from gittoxapi.differential import Differential
 from identifier.tasks_identifier.TaskIdentifier import TaskIdentifier
+from modifier.StatementModifier import StatementModifier
+import copy
 
 
 class NotSourceTask(TaskIdentifier):
@@ -9,9 +11,41 @@ class NotSourceTask(TaskIdentifier):
     def generator_prefix(self) -> str:
         return "notsource"
 
-    def process_differential(
-        self, st_getter: Callable[[int], Statement | None], i: int, diff: Differential
-    ) -> Callable[[Statement], None]:
-        if diff.file.endswith(".java"):
-            return
-        return TaskIdentifier.task_applier("NotSource", {})
+    def process_statement(
+        self, st_getter: Callable[[int], Statement | None], i: int
+    ) -> list[Statement]:
+
+        statement: Statement = st_getter(i)
+
+        if TaskIdentifier.is_task_set(statement):
+            return [statement]
+
+        if not "git" in statement.object.definition.extensions:
+            return [statement]
+
+        differentials: list[Differential] = statement.object.definition.extensions[
+            "git"
+        ]
+
+        new_statements = []
+
+        diff_i = 0
+        while diff_i < len(differentials):
+            diff = differentials[diff_i]
+            if diff.file.endswith(".java"):
+                diff_i += 1
+                continue
+            differentials.pop(diff_i)
+
+            newstatement: Statement = StatementModifier.new_statement(
+                origin=statement,
+                id=self.generator_prefix() + "~" + str(len(differentials)),
+            )
+            newdiff = copy.deepcopy(diff)
+            newstatement.object.definition.extensions["git"] = [newdiff]
+            new_statements.append(newstatement)
+            TaskIdentifier.set_task(newstatement, "NotSource", {})
+
+        statement.object.definition.extensions["git"] = differentials
+
+        return new_statements + [statement]
